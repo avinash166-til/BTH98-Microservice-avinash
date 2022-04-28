06e15b72098d661afda906924828eddc5d473f05
Avinash Tilavalli <avinash_tilavalli@thbs.india.com>
2022-04-28 17:14:28 +0530

    28/april/2022

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan("com.example.demo")
+
 @SpringBootApplication
+@ComponentScan("com.example.demo")
+@EnableJpaRepositories("com.example.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
